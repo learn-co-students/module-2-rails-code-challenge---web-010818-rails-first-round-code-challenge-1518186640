b8e14034ce53ea1a9af4854ad5736b247426c470
Anna Yang <yanganna68@gmail.com>
2018-02-09 11:00:23 -0500

    DONE

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,9 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+
+
+  def self.search(search)
+    Superhero.where("superpower_id = #{search}")
+  end
 end
