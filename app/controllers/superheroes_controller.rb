8e9e366fc5fcb101acaa9b2daa20912000bce3b1
github username <github email address>
2018-02-09 11:02:12 -0500

    code challenge

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,37 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(super_params)
+
+    if @superhero.valid?
+      redirect_to superhero_path(@superhero)
+    else
+      flash[:error] = @superhero.errors.full_messages
+      redirect_to new_superhero_path
+    end
+  end
+
+  def delete
+    @superhero = Superhero.find(params[:id])
+    @superhero.destory
+    redirect_to superheroes_path
+  end
+
+  private
+
+  def super_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
 end
