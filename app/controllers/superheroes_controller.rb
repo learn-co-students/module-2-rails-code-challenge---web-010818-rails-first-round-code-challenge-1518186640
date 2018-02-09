874d4ad25359350eafe9235cf36a3b2142a4a3e2
morgannegagne <morgannegagne@gmail.com>
2018-02-09 11:01:27 -0500

    completed code challenge

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,34 @@
 class SuperheroesController < ApplicationController
+
   def index
-    @superheroes = Superhero.all
+    if params[:search]
+      @superheroes = Superhero.search_by_superpower(params[:search])
+    else
+      @superheroes = Superhero.all
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+
+    if @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
