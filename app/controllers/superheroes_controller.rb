510e1dee2823cba71a1ff68ffb62d9daa10a622f
GuttermanA <alexanderf.gutterman@gmail.com>
2018-02-09 11:01:31 -0500

    Done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,34 @@
 class SuperheroesController < ApplicationController
   def index
-    @superheroes = Superhero.all
+    if params[:superpower]
+      @superpower = Superpower.find_by(name: params[:superpower])
+      @superheroes = Superhero.where("superpower_id = ?", @superpower.try(:id))
+    else
+      @superheroes = Superhero.all
+    end
+  end
+
+  def show
+    @superhero = Superhero.find_by(id: params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    if @superhero.save
+      redirect_to @superhero
+    else
+      flash[:message] = "Please do not leave any field blank. Super names cannot be duplicated."
+      render :new
+    end
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id, :superpower)
   end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,5 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+  validates :super_name, :name, :superpower_id, presence: true
 end
