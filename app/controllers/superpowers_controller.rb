6415753bbdb2cc63b5b7e1c1767221e92fe39925
katycarr <khcarr9@gmail.com>
2018-02-09 11:00:31 -0500

    finished

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,15 @@
 class SuperpowersController < ApplicationController
+  before_action :select_superpower, only: [:show]
   def index
     @superpowers = Superpower.all
+  end
+
+  def show
+  end
+
+  private
+
+  def select_superpower
+    @superpower = Superpower.find(params[:id])
   end
 end
