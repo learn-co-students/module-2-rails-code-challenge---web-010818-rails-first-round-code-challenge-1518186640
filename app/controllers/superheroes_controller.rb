5d8c2cfb2dd505813ea713d508832531bbfe66ec
TorreJohnson <torre.johnson@gmail.com>
2018-02-09 10:50:17 -0500

    Completed Mod 2 Code Challenge

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,38 @@
 class SuperheroesController < ApplicationController
   def index
-    @superheroes = Superhero.all
+    if params[:commit]
+      @superheroes = Superhero.all.select {|sh| sh.superpower.name.downcase == params[:q].downcase}
+    else
+      @superheroes = Superhero.all
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    if Superhero.find_by(super_name: params[:superhero][:super_name])
+      flash[:message] = "Super Name already exists"
+      redirect_to new_superhero_path
+    else
+      @superhero = Superhero.create(superhero_params)
+      redirect_to @superhero
+    end
+  end
+
+  def search
+    byebug
+  end
+
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
